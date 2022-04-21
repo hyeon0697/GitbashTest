f6e7d6f5d14a14cf9b979d78a91ead969b4b7b55
hyeon0697 <hyeon0697@gmail.com>
2022-04-21 22:58:32 +0900

    2022/04/21

--- 3week/도미노.py
+++
@@ -1,5 +0,0 @@
-count = 0
-for i in range(int(input())):
-    for j in range(i):
-        print(j)
-print(count)
--- 3week/별찍기 - 20.py
+++ 3week/별찍기 - 20.py
@@ -1,10 +1,22 @@
 count = int(input())
 oddEven = 0
-for i in range(count):
-    count -= i
+for i in range(1, count+1, 1):
     if count == 1:
         print("*", end="")
     else:
-        print("*", end=" ")
-    oddEven = 1
-    for j in range(i)
+        for j in range(1, count+1, 1):
+            if oddEven == 0 and j != count:
+                print("*", end="")
+                print(" ", end="")
+            elif oddEven == 1 and j != count:
+                print(" ", end="")
+                print("*", end="")
+            elif oddEven == 0 and j == count:
+                print("*")
+            elif oddEven == 1 and j == count:
+                print(" *")
+
+    if oddEven == 0:
+        oddEven = 1
+    else:
+        oddEven = 0
